cd3723223f664bbdc09a705d9c34e4cc9c5ad2e4
Dennis Gyurics <34282016+dgyurics@users.noreply.github.com>
2024-09-26 18:20:25 -0700

    bugfix

--- initdb/init.sql
+++ initdb/init.sql
@@ -45,8 +45,8 @@ CREATE TABLE IF NOT EXISTS product_categories (
     product_id UUID NOT NULL,
     category_id UUID NOT NULL,
     PRIMARY KEY (product_id, category_id),
-    FOREIGN KEY (product_id) REFERENCES products (id),
-    FOREIGN KEY (category_id) REFERENCES categories (id)
+    FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
 );
 
 -- Insert data into product_categories using hardcoded UUIDs
