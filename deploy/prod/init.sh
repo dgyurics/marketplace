eb9465e773e6dd63200b922715e683750c6daedb
Dennis Gyurics <34282016+dgyurics@users.noreply.github.com>
2025-08-08 18:14:44 -0700

    improve production deployment

--- deploy/prod/init.sh
+++ deploy/prod/init.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-# Generate keys
+# chmod +x deploy/prod/init.sh
+DOMAIN="marketplace.com"
+
+# Shut down all containers
+echo "Stopping all running containers..."
+docker-compose down
+
+# GENERATE RSA KEYS
 openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048
 openssl rsa -pubout -in private.pem -out public.pem
+
+# GET SSL CERTIFICATES using Let's Encrypt
+docker run --rm -v "marketplace_ssl-certs:/etc/letsencrypt" \
+    -p 80:80 \
+    certbot/certbot:v2.11.0 certonly --standalone \
+    --non-interactive --agree-tos --no-eff-email \
+    --register-unsafely-without-email \
+    -d "$DOMAIN" -d "www.$DOMAIN"
+echo "SSL certificates obtained and stored in marketplace_ssl-certs volume"
+
+# Verify certificates are in the volume
+echo "Verifying certificates in volume..."
+docker run --rm -v marketplace_ssl-certs:/certs alpine ls -la /certs/live/$DOMAIN/
+
+echo "SSL certificate setup complete!"
