47bf9854855c3169723085d6c5ddc6d16708dcc1
Dennis Gyurics <34282016+dgyurics@users.noreply.github.com>
2026-08-03 18:10:05 -0700

    disable stripe dev assistant

--- web/src/services/stripe.ts
+++ web/src/services/stripe.ts
@@ -8,7 +8,13 @@ let stripePromise: Promise<Stripe | null> | undefined
 export function getStripe() {
   if (!stripePromise) {
     loadStripe.setLoadParameters({ advancedFraudSignals: false })
-    stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY)
+    stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY, {
+      developerTools: {
+        assistant: {
+          enabled: false,
+        },
+      },
+    })
   }
   return stripePromise
 }
